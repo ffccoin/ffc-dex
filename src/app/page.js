bcf678d4598d0008a4bbc7c5151f4a2da59ac6f1
cyhammad <hammad727272@gmail.com>
2024-03-05 00:16:12 +0500

    headless added

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Swap from "@/components/home/Swap"
+import MyModal from "@/components/models/HeadlessModal"
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
         <Swap/>
+        <MyModal />
     </main>
     )
 }
