135bca3df862d5c14dfc97ec1a94d85d0331b7e2
ramisha <ramishashahid22@gmail.com>
2024-03-16 15:52:52 +0500

    img

--- src/blockchain/config/index.js
+++ src/blockchain/config/index.js
@@ -14,6 +14,7 @@ const metadata = {
   url: "https://web3modal.com", // origin must match your domain & subdomain
   icons: ["https://avatars.githubusercontent.com/u/37784886"],
 };
+ 
 
 // Create wagmiConfig
 export const wagmiConfig = defaultWagmiConfig({
