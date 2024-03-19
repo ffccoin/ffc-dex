f48ab5a2b07487393b6ae567a96ef181af183ebb
cyhammad <hammad727272@gmail.com>
2024-03-19 06:21:21 +0500

    send almost done

--- src/app/send/page.jsx
+++ src/app/send/page.jsx
@@ -1,12 +1,145 @@
+"use client";
+
+import LinkedParticlesAnimation from "@/components/animations/LinkedParticlesAnimation";
 import HomeHeader from "@/components/headers/HomeHeader";
-import React from "react";
+import SwapBalance from "@/components/home/swap/SwapBalance";
+import SelectATokenSendModal from "@/components/models/SelectATokenSendModal";
+import { useWeb3ModalState } from "@web3modal/wagmi/react";
+import React, { useEffect, useState } from "react";
 
 const SendPage = () => {
+  const [amount, setAmount] = useState(null);
+  const [isOpen, setIsOpen] = useState(false);
+  const [token, setToken] = useState(null);
+  const [changeToken, setChangeToken] = useState(1);
+  const { address } = useWeb3ModalState();
+  const [walletAddressToSendTokens, setWalletAddressToSendTokens] =
+    useState("");
+  const handleAmountChange = (e) => {
+    setAmount(e.target.value);
+  };
+
+  const handleWalletAddressToSendTokensChange = (e) => {
+    setWalletAddressToSendTokens(e.target.value);
+  };
+  function openModal(asset) {
+    setChangeToken(asset);
+    setIsOpen(true);
+  }
+
   return (
-    <div className="overflow-hidden h-full flex flex-col items-center justify-center mt-20">
-      <HomeHeader noSettings />
+    <div className="overflow-hidden h-full flex items-center justify-center px-4 relative">
+      <LinkedParticlesAnimation />
+      <div className="bg-gray22/50 z-50 py-4 sm:px-2 px-4 rounded-2xl flex flex-col gap-y-1 items-center justify-center w-full max-w-[512px] mx-4">
+        <HomeHeader noSettings />
+        <SelectATokenSendModal
+          isOpen={isOpen}
+          setIsOpen={setIsOpen}
+          token={token}
+          setToken={setToken}
+          tokenAmount={amount}
+          setTokenAmount={setAmount}
+          changeToken={changeToken}
+        />
+        <div className="flex flex-col gap-y-[2px] w-full h-full mt-3">
+          <div className="flex flex-col bg-gray22/80 rounded-t-lg pb-10">
+            <p className="text-gray10  px-3 py-2">You are sending</p>
+            <div className="flex justify-center">
+              <div className="flex flex-col gap-y-1">
+                <input
+                  className="bg-transparent text-center w-full outline-none text-7xl"
+                  placeholder="$0"
+                  name="amount"
+                  id="amount"
+                  autoComplete="off"
+                  value={amount}
+                  onChange={handleAmountChange}
+                />
+                {token ? (
+                  <p className="text-center text-gray10">0{token?.symbol}</p>
+                ) : (
+                  <p className="text-center text-gray10">
+                    Please select a token
+                  </p>
+                )}
+              </div>
+            </div>
+          </div>
+          <div className="bg-gray22/80 rounded-b-lg">
+            {token ? (
+              <div className="flex items-center w-full gap-x-2">
+                <button
+                  className="text-start flex gap-x-1 w-full items-center justify-between p-3 border border-gray22 rounded-2xl h-[64px] min-w-[136px]"
+                  onClick={() => openModal(1)}
+                >
+                  <img
+                    src={token.logoURI}
+                    alt="assetOneLogo"
+                    className="assetLogo"
+                    width={32}
+                    height={32}
+                    quality={100}
+                  />
+                  <div className="flex flex-col w-full px-2">
+                    <span className="text-sm font-semibold">
+                      {token.symbol}
+                    </span>
+                    {token && address ? (
+                      <SwapBalance address={address} token={token.address} />
+                    ) : (
+                      <span className="text-sm text-gray10">Balance: 0.0</span>
+                    )}
+                  </div>
+                  {chevronDown}
+                </button>
+              </div>
+            ) : (
+              <div className="">
+                <button
+                  className="text-start justify-self-end p-4 w-full text-gray10"
+                  onClick={() => openModal(1)}
+                >
+                  Select a token
+                </button>
+              </div>
+            )}
+          </div>
+        </div>
+        <div className="rounded-lg bg-gray22/80 p-3 flex w-full flex-col gap-y-1">
+          <span className="text-sm text-gray10">To</span>
+          <input
+            type="text"
+            className="bg-transparent outline-none placeholder:text-gray15 w-full"
+            placeholder="Wallet Address"
+            value={walletAddressToSendTokens}
+            onChange={handleWalletAddressToSendTokensChange}
+            autoComplete="off"
+          />
+        </div>
+        <button className="rounded-lg bg-gray22/80 text-center py-4 text-xl font-bold w-full gap-y-1">
+          {walletAddressToSendTokens === "" ? "Select recipient" : "Send"}
+        </button>
+      </div>
     </div>
   );
 };
+
+const chevronDown = (
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M8 9.99997L12 14L16 9.99997"
+      stroke="white"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
 
 export default SendPage;
---
+++ src/components/models/SelectATokenSendModal.jsx
@@ -0,0 +1,121 @@
+"use client";
+
+import { Dialog } from "@headlessui/react";
+import { useEffect, useState } from "react";
+import tokenList1 from "../../lists/tokenList.json";
+import tokenList56 from "../../lists/chain56.json";
+import { useWeb3ModalState } from "@web3modal/wagmi/react";
+import Image from "next/image";
+
+const SelectATokenSendModal = ({
+  isOpen,
+  setIsOpen,
+  setToken,
+  setTokenAmount,
+}) => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [tokenList, setTokenList] = useState([]);
+  const { selectedNetworkId } = useWeb3ModalState();
+  const filteredTokenList = tokenList.filter((token) => {
+    const tokenName = token.symbol.toLowerCase();
+    const tokenAddress = token.address.toLowerCase();
+    const search = searchQuery.trim().toLowerCase();
+    return tokenName.includes(search) || tokenAddress.includes(search);
+  });
+
+  useEffect(() => {
+    setToken(null);
+    setTokenAmount(0);
+    if (selectedNetworkId == 56) {
+      setTokenList(tokenList56);
+    } else {
+      setTokenList(tokenList1);
+    }
+  }, [selectedNetworkId]);
+  function modifyToken(i) {
+    setToken(filteredTokenList[i]);
+    setTokenAmount(null);
+    setIsOpen(false);
+  }
+
+  return (
+    <Dialog
+      open={isOpen}
+      onClose={() => setIsOpen(false)}
+      className="relative z-50"
+    >
+      <div className="fixed inset-0 backdrop-blur-sm" aria-hidden="true" />
+      <div className="fixed inset-0 flex items-center justify-center p-10">
+        <Dialog.Panel className="flex bg-gray23 rounded-3xl w-full max-w-[32rem] h-[95%] px-7 py-5">
+          <div className="flex flex-col w-full">
+            <div className="flex mt-4 items-center justify-between">
+              <div className="flex-grow text-center">
+                <p className="text-2xl font-light">Select a Token</p>
+              </div>
+              <Image
+                alt="close"
+                src="/home/cross.svg"
+                className="justify-self-end cursor-pointer"
+                width={24}
+                height={24}
+                onClick={() => setIsOpen(false)}
+              />
+            </div>
+            <input
+              type="text"
+              placeholder="Search name or paste address"
+              className="w-full p-2 mt-6 mb-2 border h-14 outline-none rounded-2xl border-gray22 text-neutralLight bg-gray23"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+            />
+            <div className="overflow-y-auto h-full px-2 scrollbar-hidden">
+              {filteredTokenList.map((e, i) => {
+                return (
+                  <Dialog.Description className="mt-4" key={i}>
+                    <div
+                      className="flex gap-9 items-center justify-between cursor-pointer"
+                      onClick={() => modifyToken(i)}
+                    >
+                      <div className="flex gap-5 items-center">
+                        <img
+                          src={e.logoURI}
+                          alt={e.symbol}
+                          width={33}
+                          height={33}
+                          quality={100}
+                        />
+                        <div className="flex flex-col">
+                          <div className="text-base uppercase font-neue-machina">
+                            {e.symbol}
+                          </div>
+                          <div className="text-sm mt-1 text-neutralLight">
+                            {e.name}
+                          </div>
+                        </div>
+                      </div>
+
+                      <div className="text-sm text-neutralLight">
+                        {e.decimals}
+                      </div>
+                    </div>
+                  </Dialog.Description>
+                );
+              })}
+            </div>
+            <div className="flex justify-center gap-3 py-2">
+              <Image
+                alt="manage"
+                src="/home/manage.svg"
+                width={24}
+                height={24}
+              />
+              <p className="text-primary1">Manage</p>
+            </div>
+          </div>
+        </Dialog.Panel>
+      </div>
+    </Dialog>
+  );
+};
+
+export default SelectATokenSendModal;
