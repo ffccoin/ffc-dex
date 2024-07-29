51b86e9e8aab3bfcdf44bce278feed0d3e2ca1e4
cyhammad <hammad727272@gmail.com>
2024-07-29 21:52:25 +0500

    error added

---
+++ src/app/error/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ErrorPage = () => {
+  return (
+    <div className="flex flex-col h-screen items-center justify-center">
+      <h1 className="text-4xl font-bold">Error 404</h1>
+      <p className="text-xl">Contact Administrator</p>
+    </div>
+  );
+};
+
+export default ErrorPage;
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -14,13 +14,17 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <StoreProvider>
+        <div className="flex flex-col h-screen items-center justify-center">
+          <h1 className="text-4xl font-bold">Error 404</h1>
+          <p className="text-xl">Contact Administrator</p>
+        </div>
+        {/* <StoreProvider>
           <Web3Modal>
             <PreloaderProvider>
               {children}
             </PreloaderProvider>
           </Web3Modal>
-        </StoreProvider>
+        </StoreProvider> */}
       </body>
     </html>
   );
