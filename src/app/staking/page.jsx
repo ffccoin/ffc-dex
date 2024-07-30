6dc6bd7674cdc68c11dfb2e5403db869a0229935
cyhammad <hammad727272@gmail.com>
2024-07-31 00:39:06 +0500

    fixed

--- src/app/staking/page.jsx
+++ src/app/staking/page.jsx
@@ -5,7 +5,7 @@ import React from "react";
 
 export default function page() {
   return (
-    <div className=" absolute  md:relative md:top-0 top-[5.25rem] z-50 py-4 px-4 rounded-2xl flex  md:flex-row flex-col-reverse gap-x-[3vw] items-center md:justify-center  ">
+    <div className="md:relative md:top-0 top-[5.25rem] z-50 py-4 px-4 rounded-2xl flex  md:flex-row flex-col-reverse gap-x-[3vw] items-center md:justify-center">
       <Section1 />
       <Staking />
     </div>
